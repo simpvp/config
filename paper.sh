2072b7084cd468833e7fedc4a7f275b58508c7e8
Simpvp admins <github-admins@simplicitypvp.net>
2020-10-27 03:01:56 +0100

    Update paper
    This should hopefully fix the "portal lag"

--- paper.sh
+++ paper.sh
@@ -1,1 +1,1 @@
-java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -Xmx20G -jar paper-1.15.2-387.jar
+nice -n -20 -- java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -jar paper-1.15.2-389.jar
