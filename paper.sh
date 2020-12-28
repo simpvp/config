aec5188f16eee33f78fb58f1d46e8d68f6fd0562
Simpvp admins <github-admins@simplicitypvp.net>
2020-12-28 17:04:10 +0000

    Update to 1.16.4

--- paper.sh
+++ paper.sh
@@ -1,1 +1,1 @@
-nice -n -20 -- java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -jar paper-1.15.2-389.jar
+nice -n -20 -- java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true -jar paper-1.16.4-348.jar
