8445e3cac271225dcf6414c7bb56f6632f6ef36e
Simpvp admins <github-admins@simplicitypvp.net>
2022-02-08 04:53:35 +0000

    Update paper

--- bin/paper.sh
+++ bin/paper.sh
@@ -11,4 +11,4 @@ fi
 exec nice -n -20 -- \
 	java -server -Dlog4j.configurationFile=./Log4j2.xml -DIReallyKnowWhatIAmDoingISwear=true \
 	-Xms6G -Xmx32G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
-	-jar ./bin/paper-1.17.1-400.jar
+	-jar ./bin/paper-1.17.1-401.jar
