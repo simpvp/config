132b85852976b254615493c9abb82d03e32c406d
Simpvp admins <github-admins@simplicitypvp.net>
2022-02-08 04:53:22 +0000

    Update start.sh
    better locking mechanism

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 set -euo pipefail
 
-# Server will not automatically restart within this many seconds of already restarting
-RESTART_LIMIT=60
+get_lock() {
+	if [ -f ./lock ]; then
+		LOCK=$(< ./lock)
+		if [[ "$LOCK" == "$PPID" ]]; then
+			echo "Already had lock on current PID"
+			return
+		fi
+
+		if  [ -d "/proc/$LOCK" ]; then
+			echo "Cannot start the server as another process is still running. Ensure the other server process is stopped before starting a new one."
+			echo "Running PID: $LOCK"
+			return 2
+		else
+			echo "Already had lock file for PID $LOCK."
+			echo "No such process found, overwriting lock."
+		fi
+	fi
+
+	echo -n "$PPID" > ./lock
+	echo "Created lock for pid $PPID"
+}
+
+delete_lock() {
+	LOCK=$(< ./lock) || return 0
+	if [[ "$LOCK" == "$PPID" ]]; then
+		rm ./lock
+	else
+		echo "Unable to delete lockfile. Got $LOCK != $PPID"
+		return 1
+	fi
+}
+
+exit() {
+	delete_lock
+}
+trap exit EXIT
 
 SCRIPT_DIR=$(readlink -f $(dirname "$0"))
 PWD=$(pwd)
@@ -11,28 +45,19 @@ if [[ "$SCRIPT_DIR" != "$PWD/bin" ]]; then
 	exit 1
 fi
 
+# Server will not automatically restart within this many seconds of already restarting
+RESTART_LIMIT=60
+
 while true
 do
 	TIME=$(date -u +%s)
 	mv staging/* plugins/ || true
-	if [ -f ./lock ]; then
-		echo "Cannot start the server as another process is still running. Ensure the other server process is stopped before starting a new one."
-		echo "Running PID: $(< ./lock)"
-		exit 2
-	fi
 
-	echo -n "$PPID" > ./lock
+	get_lock
 
 	./bin/bwrap.sh || true
 
-
-	LOCK=$(< ./lock)
-	if [[ "$LOCK" == "$PPID" ]]; then
-		rm ./lock
-	else
-		echo "Unable to delete lockfile. Got $LOCK != $PPID"
-		exit 1
-	fi
+	delete_lock
 
 	if [ "$TIME" -ge "$(expr "$(date -u +%s)" - "$RESTART_LIMIT")" ]; then
 		echo "Server trying to restart within $RESTART_LIMIT seconds. This is too soon, shutting down."
