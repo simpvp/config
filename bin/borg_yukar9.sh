e8b002ac4ef0d5de2554602fed8388a1eeea11cc
Simpvp admins <github-admins@simplicitypvp.net>
2025-10-25 08:57:14 +0000

    Update

--- bin/borg_yukar9.sh
+++ bin/borg_yukar9.sh
@@ -28,16 +28,21 @@ export BORG_REPO='ssh://borg-yukar9/mnt/borg/simpvp'
 # If arguments are supplied then run those with borg with the right environment
 # variables set, letting you do for example ./borg.sh list
 if [ "$#" -gt 0 ]; then
-	echo "borg $@"
+	echo "borg $@" >/dev/stderr
 	borg "$@"
 	exit
 fi
 
-borg create -v --stats "${PROGRESS[@]}" \
+RET=0
+
+if ! borg create -v --stats "${PROGRESS[@]}" \
 	--compression lzma,6 \
 	--upload-ratelimit 25000 \
 	::'mc_by_{hostname}_{utcnow}_UTC_Borg_{borgversion}' \
-	/home/mc
+	/home/mc; then
+	RET=1
+	echo "borg create returned error status" >/dev/stderr
+fi
 
 borg prune -v --list \
 	--keep-within=60d \
@@ -45,3 +50,5 @@ borg prune -v --list \
 	--keep-monthly=10000 \
 	--keep-yearly=10000 \
 	--glob-archives 'mc_*'
+
+exit "$RET"
